84d2a3412c4f2a0fb3fc091bffd88ee59f8275d1
Yuki Hayashi <mrchildreny@gmail.com>
2024-06-15 15:12:19 +0900

    feat: Update content script to use lazy loading for images

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,6 +1,7 @@
 import "flowbite/dist/flowbite.css";
 
 import PrTitleGenerateButton from '@/app/components/PrTitleGenerateButton/PrTitleGenerateButton.svelte';
+import { waitForElement } from "./waitForElement";
 
 console.log("Notion To Github PR Title loaded");
 
@@ -35,33 +36,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
   }
 });
-
-function waitForElement(selector: string): Promise<HTMLElement> {
-  return new Promise((resolve) => {
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        const element = document.querySelector(selector);
-        if (element) {
-          observer.disconnect();
-          resolve(element as HTMLElement);
-          break;
-        }
-      }
-    });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true
-    });
-
-    // なぜかうまく動かないのでコメントアウトする
-    // 既に存在する場合は即座に解決
-    const element = document.querySelector(selector);
-    if (element) {
-      observer.disconnect();
-      resolve(element as HTMLElement);
-    }
-  });
-}
 
 mountGenerateButton();
