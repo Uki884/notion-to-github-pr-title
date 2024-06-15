5cee994f367090741ebc275e6bb3809a89c8e514
Yuki Hayashi <mrchildreny@gmail.com>
2024-06-15 13:46:01 +0900

    flowbiteを入れる

--- src/app/components/Options/Options.svelte
+++ src/app/components/Options/Options.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { storage } from "@/app/lib/storage";
-
+  import { Button, Input, Label, Helper, Alert } from 'flowbite-svelte';
   export let authToken: string = "";
   export let databaseId: string = "";
 
@@ -8,7 +8,7 @@
 
   function save() {
     storage.set({ authToken, databaseId }).then(() => {
-      successMessage = "Options saved!";
+      successMessage = "保存しました";
       setTimeout(() => {
         successMessage = null;
       }, 1500);
@@ -17,50 +17,45 @@
 </script>
 
 <div class="container">
-  <div>
-    <label for="authToken">Notion API Key</label>
-    <input
-      type="text"
-      id="authToken"
-      bind:value={authToken}
-      placeholder="enter your token"
-    />
-  </div>
-  <div>
-    <label for="databaseId">Notion DataBase ID</label>
-    <input
-      type="text"
-      id="databaseId"
-      bind:value={databaseId}
-      placeholder="enter your database id"
-    />
-  </div>
-  <button on:click={save}>Save</button>
-  {#if successMessage}<span class="success">{successMessage}</span>{/if}
+  {#if successMessage}
+    <Alert color="green">
+      <span class="font-medium">{ successMessage }</span>
+    </Alert>
+  {/if}
+
+  <form class="forms mt-4">
+    <div>
+      <Label for="authToken" class="mb-2">Notion API Key</Label>
+      <Input bind:value={authToken} type="text" id="authToken" required placeholder="APITokenを入力してください" />
+      <Helper class="mt-2">
+        <p>NotionAPIキーは<a href="https://www.notion.so/my-integrations" target="_blank">こちら</a>から入手できます</p>
+        <p>例: 「secret_****************************」</p>
+      </Helper>
+    </div>
+
+    <div>
+      <Label for="databaseId" class="mb-2">Notion Task DataBase ID</Label>
+      <Input bind:value={databaseId} type="text" id="databaseId" required placeholder="Database IDを入力してください" />
+      <Helper class="mt-2">
+        <p>例: https://www.notion.so/5f13a2e971ac4cec98cef9fb83599f63pの場合は「5f13a2e971ac4cec98cef9fb83599f63p」がIDになります</p>
+      </Helper>
+    </div>
+
+    <Button on:click={save} size="lg" color="dark" class="w-50">保存</Button>
+
+  </form>
 </div>
 
 <style>
   .container {
-    min-width: 250px;
+    min-width: 500px;
+    padding: 20px;
   }
 
-  button {
-    border-radius: 2px;
-    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.6);
-    background-color: #2ecc71;
-    color: #ecf0f1;
-    transition: background-color 0.3s;
-    padding: 5px 10px;
-    border: none;
+  .forms {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
   }
 
-  button:hover,
-  button:focus {
-    background-color: #27ae60;
-  }
-
-  .success {
-    color: #2ecc71;
-    font-weight: bold;
-  }
 </style>
--- src/options/index.ts
+++ src/options/index.ts
@@ -1,5 +1,6 @@
 import Options from "@/app/components/Options/Options.svelte";
 import { storage } from "@/app/lib/storage";
+import "flowbite/dist/flowbite.css";
 
 function render() {
   const target = document.getElementById("app");
